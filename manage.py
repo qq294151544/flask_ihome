33052bd5c52d855a12112a482c03ae2269ccfa9a
xwp <xwp_fullstack@163.com>
2018-05-13 18:21:45 +0800

    Revert "flask_ihome 3.项目创建 创建redis连接"

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import redis
+
 app = Flask(__name__)
 
 
@@ -11,17 +12,19 @@ class Config(object):
     # 数据库配置
     SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1/ihome'
     SQLAlCHEMY_TRACK_MODIFICATION = False
+    REDIS_HOST = '127.0.0.1'
+    REDIS_PORT = 6379
 
 
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+
 
 @app.route('/')
 def index():
     return 'hello'
 
-
-# ggggg
 
 if __name__ == '__main__':
     app.run(debug=True)

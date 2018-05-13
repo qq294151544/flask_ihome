b9db738686f3c8abaff97b4fa49bd4d07e9f6a5a
xwp <xwp_fullstack@163.com>
2018-05-13 18:27:55 +0800

    flask_ihome 4.项目创建 开启csrf防护

--- manage.py
+++ manage.py
@@ -2,13 +2,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import redis
-
+from flask_wtf import CSRFProtect
 app = Flask(__name__)
 
 
 class Config(object):
     '''工程配置信息'''
     DEBUG = True
+    SECRECT_KEY = '1+LWdsiotKtsOYb9/frWRRw0JGLlQUsmLn36Foxp4+p0clIIHhjnoIY1iGR1UoIt'
     # 数据库配置
     SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1/ihome'
     SQLAlCHEMY_TRACK_MODIFICATION = False
@@ -19,7 +20,7 @@ class Config(object):
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
-
+CSRFProtect(app)
 
 @app.route('/')
 def index():

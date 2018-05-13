547efe96bfea7a7738d9d52d3a501d493501b5c1
xwp <xwp_fullstack@163.com>
2018-05-13 18:05:19 +0800

    flask_ihome 2.项目创建 连接数据库

--- manage.py
+++ manage.py
@@ -1,13 +1,28 @@
 # coding=utf-8
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+
+class Config(object):
+    '''工程配置信息'''
+    DEBUG = True
+    # 数据库配置
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1/ihome'
+    SQLAlCHEMY_TRACK_MODIFICATION = False
+
+
+app.config.from_object(Config)
+db = SQLAlchemy(app)
 
 
 @app.route('/')
 def index():
     return 'hello'
-#ggggg
+
+
+# ggggg
 
 if __name__ == '__main__':
     app.run(debug=True)

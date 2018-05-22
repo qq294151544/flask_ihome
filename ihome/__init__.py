0e425a886e4bd652efaa9898c3ea92b99260fc95
xwp <xwp_fullstack@163.com>
2018-05-22 17:25:45 +0800

    flask_ihome 10.项目创建 获取网站图标

--- ihome/__init__.py
+++ ihome/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
-
+from ihome.utils.commons import RegexConvter
 from config import config_dict
 
 db = SQLAlchemy()
@@ -22,12 +22,18 @@ def create_app(config_name):
     # 创建redis数据窟链接对象
     redis_store = redis.StrictRedis(host=config_cls.REDIS_HOST, port=config_cls.REDIS_PORT)
 
+    # 开启csrf防护
     CSRFProtect(app)
 
+    # session信息的储存
     Session(app)
-    #注册蓝图对象
+
+    # 添加路由转换器
+    app.url_map.converters['re'] = RegexConvter
+
+    # 注册蓝图对象
     from ihome.api_1_0.index import api
-    app.register_blueprint(api,url_prefix='/api/v1.0')
+    app.register_blueprint(api, url_prefix='/api/v1.0')
 
     from ihome.web_html import html
     app.register_blueprint(html)
--- ihome/web_html.py
+++ ihome/web_html.py
@@ -2,11 +2,19 @@
 # 该蓝图给刘看齐提供静态页面
 from flask import Blueprint
 from flask import current_app
+
 html = Blueprint('html', __name__)
 
 
-@html.route('/<file_name>')
+# @html.route('/<file_name>')
+@html.route("/<re('.*'):file_name>")
 def get_static_html(file_name):
     # 获取静态文件目录下的静态文件内容并返回给浏览器
-    file_name = 'html/'+file_name
+    if file_name == '':
+        # 如果用户访问路径为空，则默认访问首页，返回index.html
+        file_name = 'index.html'
+    if file_name != 'favicon.ico':
+        # 当浏览器访问一个网站的时候，浏览器会自动访问网站下的一个favicon文件，网站图标
+        file_name = 'html/' + file_name
+
     return current_app.send_static_file(file_name)

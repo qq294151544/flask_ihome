08bb7e30765324b97eeb1be09bc7c99a5fc37f6b
xwp <xwp_fullstack@163.com>
2018-05-22 22:21:58 +0800

    flask_ihome 13.项目集成日志功能添加

--- ihome/__init__.py
+++ ihome/__init__.py
@@ -6,9 +6,24 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 from ihome.utils.commons import RegexConvter
 from config import config_dict
+import logging
+from logging.handlers import RotatingFileHandler
 
 db = SQLAlchemy()
 redis_store = None
+
+
+def set_logging(log_level):
+    # 设置日志的记录等级
+    logging.basicConfig(level=logging.DEBUG)  # 调试debug级
+    # 创建日志记录器，指明日志保存的路径、每个日志文件的最大大小、保存的日志文件个数上限
+    file_log_handler = RotatingFileHandler('logs/log', maxBytes=1024 * 1024 * 100, backupCount=10)
+    # 创建日志记录的格式
+    formatter = logging.Formatter('%(levelname)s %(filename)s:%(lineno)d %(message)s')
+    # 为刚创建的日志记录器设置日志记录格式
+    file_log_handler.setFormatter(formatter)
+    # 为全局的日志工具对象（flask app使用的）添加日志记录器
+    logging.getLogger().addHandler(file_log_handler)
 
 
 # 工厂方法：传入不同的参数，返回不同的对象
--- ihome/api_1_0/index.py
+++ ihome/api_1_0/index.py
@@ -1,8 +1,10 @@
-
-#coding=utf-8
+# coding=utf-8
 from . import api
+from flask import current_app
+
+
 # 2、使用蓝图对象注册路由
 @api.route('/', methods=['GET', 'POST'])
 def index():
-
+    # current_app.logger.fatal('Fatal Message')
     return 'hello'

95c14e9344249f896eef110948af873dced37a31
xwp <xwp_fullstack@163.com>
2018-06-04 18:12:55 +0800

    flask_ihome 25.login_required登陆验证装饰器，g变量保存user_id 修改

--- ihome/utils/commons.py
+++ ihome/utils/commons.py
@@ -1,5 +1,7 @@
 # coding=utf-8
-from flask import session, jsonify,g
+import functools
+
+from flask import session, jsonify, g
 from werkzeug.routing import BaseConverter
 
 from ihome.utils.response_code import RET
@@ -16,17 +18,18 @@ class RegexConvter(BaseConverter):
 
 # 自定义登陆验证装饰器
 def login_required(view_func):
+    @functools.wraps(view_func)
     def wrapper(*args, **kwargs):
         # 进行登陆验证
         user_id = session.get('user_id')
         if user_id:
             # 用户已经登陆，调用视图函数
             # 使用g变量临时保存登陆用户的id，g变量中的内容可以在每次请求开始到请求范围内使用
-            #之所以使用g变量临时保存登陆用户的id，是为了在视图中获取user_id时候不再取session中读取
-            g.user_id=user_id
+            # 之所以使用g变量临时保存登陆用户的id，是为了在视图中获取user_id时候不再取session中读取
+            g.user_id = user_id
             return view_func(*args, **kwargs)
         else:
             # 用户未登录
             return jsonify(errno=RET.SESSIONERR, errmsg='用户未登录')
 
-    return wrapper()
+    return wrapper

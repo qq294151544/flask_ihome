510f768c8d334ea7d01e11c7339c55817c2feee4
xwp <xwp_fullstack@163.com>
2018-06-04 17:04:42 +0800

    flask_ihome 24.个人信息页面显示 用户名与头像

--- ihome/static/js/ihome/my.js
+++ ihome/static/js/ihome/my.js
@@ -5,11 +5,24 @@ function getCookie(name) {
 
 // TODO: 点击推出按钮时执行的函数
 function logout() {
-    
+
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
 
     // TODO: 在页面加载完毕之后去加载个人信息
+    $.get('/api/v1.0/user', function (resp) {
+        if (resp.errno == '0') {
+            //获取个人信息成功
+            //设置用户个人头像img的src属性
+            $('#user-avatar').attr('src', resp.data.avatar_url)
+            $('#user-name').html(resp.data.username);
+            $('#user-mobile').html(resp.data.mobile);
+        }
+        else {
+            //获取个人信息失败
+            alert(resp.errmsg);
+        }
+    })
 
 });
